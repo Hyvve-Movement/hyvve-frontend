edc4792146f5299196c5e972b164edbab40a7f3b
0-th <artemisfowlx@protonmail.com>
2025-02-23 15:08:19 +0100

    feat: aptos ts sdk integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { WalletProvider } from "@/helpers/WalletProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WalletProvider>
+        <Component {...pageProps} />
+      </WalletProvider>
+    </QueryClientProvider>
+  );
 }
